06941168c05995d03cb540dbb6861716a78dd70a
RazorHollow <rob@razorhollow.com>
2024-01-16 15:40:21 -0500

    update db with job model and seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -40,6 +40,34 @@ async function seed() {
     },
   });
 
+  await prisma.job.create({
+    data: {
+      caseName: "Smith v. Jones",
+      jobDate: new Date('2024-01-01T00:00:00.000Z'),
+      dueDate: new Date('2024-01-15T00:00:00.000Z'),
+      client: "Barclay Damon",
+      userId: user.id
+    }
+  })
+  await prisma.job.create({
+    data: {
+      caseName: "Porchek v. Wales",
+      jobDate: new Date('2024-01-10T00:00:00.000Z'),
+      dueDate: new Date('2024-01-25T00:00:00.000Z'),
+      client: "Ziff",
+      userId: user.id
+    }
+  })
+  await prisma.job.create({
+    data: {
+      caseName: "Roe v. Wade",
+      jobDate: new Date('1971-12-13T00:00:00.000Z'),
+      dueDate: new Date('1973-01-21T00:00:00.000Z'),
+      client: "Dallas County",
+      userId: user.id
+    }
+  })
+
   console.log(`Database has been seeded. 🌱`);
 }
 
