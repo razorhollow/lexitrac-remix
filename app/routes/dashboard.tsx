f1db69afab2072d6ccd36871ebe55430ac8ca3ac
RazorHollow <rob@razorhollow.com>
2024-01-25 15:20:53 -0500

    add tailwind sidebar skeleton

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,11 +1,12 @@
 import { Link, Outlet } from "@remix-run/react";
 
 import { Button } from "~/components/ui/Button";
+import SideBarComponent from "~/components/ui/SideBar";
 
 export default function Dashboard() {
   return (
     <div>
-      <h1>This is the Dashboard</h1>
+      <SideBarComponent />
       <Link to="new"><Button>Add +</Button></Link>
       <Outlet />
     </div>
