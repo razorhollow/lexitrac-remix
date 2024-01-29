34c51424fa35830c22228bde6913664590354522
razorhollow <rob@razorhollow.com>
2024-01-29 18:17:04 -0500

    reimport userUser

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -4,6 +4,7 @@ import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
 
 import { getNoteListItems } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
+import { useUser } from "~/utils";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
